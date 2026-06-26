d672997afb76192bd322276e4f36db6942e94516
trigusf <trigusf@gmail.com>
2026-06-26 22:53:49 +0700

    menambah tailwindcss dalam project

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,9 @@
 import { createClient  } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonkey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(
     supabaseUrl,
-    supabaseAnonkey
+    supabasePublishableKey
 );
